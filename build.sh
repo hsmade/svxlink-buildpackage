13d938e28aea55ec6db960b553ae4818a1ed3cab
wfournie <wim.fournier@oracle.com>
2016-05-26 09:52:06 +0200

    fix for build script

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+cd /
+
 if ([ -n "$1" ] && echo "$1" |egrep -q https?://)
 then
     git clone "$1" svxlink || exit 1
@@ -6,13 +8,14 @@ then
 fi
 
 test -d /builds/hsmade/svxlink && mv /builds/hsmade/svxlink /svxlink
-test -d svxlink/src || (echo Either mount the source as /svxlink or give me a GIT url && exit 1)
+test -d /svxlink/src || echo Either mount the source as /svxlink or give me a GIT url && exit 1
 
 mkdir build
 cd build
+
 cmake -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_INSTALL_SYSCONFDIR=/etc \
       -DCMAKE_INSTALL_LOCALSTATEDIR=/var \
-      -DCMAKE_BUILD_TYPE=Release ../svxlink/src
+      -DCMAKE_BUILD_TYPE=Release /svxlink/src
 make
 
 useradd -g daemon svxlink
